656f060bcc83ac700f09a377119435c64f5e7439
uuganbold <uuganhuu@gmail.com>
2019-09-27 23:06:50 -0500

    Bootstrap data - closes #12

--- src/main/java/edu/luc/comp433/model/Address.java
+++ src/main/java/edu/luc/comp433/model/Address.java
@@ -1,26 +1,34 @@
 package edu.luc.comp433.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Address {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     private String country;
 
+    @NotBlank
     private String street;
 
     private String unit;
 
+    @NotBlank
     private String city;
 
     private String state;
